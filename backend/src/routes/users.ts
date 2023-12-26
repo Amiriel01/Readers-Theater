b09308ec653271f693c1c8a4ced89c34e20ca93a
Sarah Quinney <quinney.sarah@gmail.com>
2023-12-26 12:45:01 -0600

    set up skeleton frontend and backend for project

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,39 @@
+import {Router} from "express";
+const router = Router();
+import passport from 'passport';
+import { user_list, user_get, user_create } from "../controllers/userController.ts";
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+//GET user list
+router.get("/userList", user_list());
+
+//GET user details
+router.get("/user/:id", user_get());
+
+//POST new user
+router.post("/userCreate", user_create());
+
+//Passport Login
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login"
+  })
+);
+
+//Passport Logout
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/")
+  });
+});
+
+export default router;
