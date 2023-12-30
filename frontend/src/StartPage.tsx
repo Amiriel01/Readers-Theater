8a216e2ef4c6e7a8382dbd771bf27e96d95cecf6
Sarah Quinney <quinney.sarah@gmail.com>
2023-12-30 10:24:24 -0600

    start page fully responsive

--- frontend/src/StartPage.tsx
+++ frontend/src/StartPage.tsx
@@ -18,12 +18,14 @@ export default function StartPage() {
                             A social media platform for readers to share their thoughts on books, authors, and more.
                         </Col>
                     </Row>
-                    <Link to='/Login'>
-                        <MyButton id='start-page-button1' title='Login'></MyButton>
-                    </Link>
-                    <Link to='/SignUp'>
-                        <MyButton id='start-page-button2' title='Sign Up'></MyButton>
-                    </Link>
+                    <div id='start-page-buttons-container'>
+                        <Link to='/Login'>
+                            <MyButton id='start-page-button1' title='Login'></MyButton>
+                        </Link>
+                        <Link to='/SignUp'>
+                            <MyButton id='start-page-button2' title='Sign Up'></MyButton>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
