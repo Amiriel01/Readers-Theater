7c9a2b221cd28d717330ba4b44a35a28935d8c37
Sarah Quinney <quinney.sarah@gmail.com>
2024-02-07 22:16:05 -0600

    fixed TypeScript, fixed UpdateProfile erasing images

---
+++ frontend/src/models/user.model.ts
@@ -0,0 +1,10 @@
+// import { UserInterface } from "../interfaces/user.interface";
+
+// export class UserModel implements UserInterface {
+//     _id: string = '';
+//     username: string = '';
+//     profile_name: string = '';
+//     about_section: string = '';
+//     imageURL: string = '';
+//     friends: UserInterface[] = [];
+// }
